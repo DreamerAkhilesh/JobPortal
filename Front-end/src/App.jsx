437fdf9711d92d52f5babc8a889f66fb3b8101ff
DreamerAkhilesh <imakhileshpratap@gmail.com>
2025-02-01 23:11:39 +0530

    Few more additions New files added

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import Home from './components/Home';
 import Jobs from './components/Jobs';
+import Browse from './components/Browse';
 
 
 const appRouter = createBrowserRouter([
@@ -23,6 +24,14 @@ const appRouter = createBrowserRouter([
   {
     path:'/jobs',
     element:<Jobs/>
+  },
+  {
+    path:'/browse',
+    element:<Browse/>
+  },
+  {
+    path:'/profile',
+    element:<Profile/>
   },
 ]);
 
